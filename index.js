5739ac6928f8b5393116a511634622aa66aad382
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-20 19:25:25 +0530

    frontend html

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const btn = document.querySelector("#submit_btn");
-
 const inp = document.querySelector(`input[type="number"]`);
 const res = document.querySelector("#result_portion");
 
@@ -17,13 +16,20 @@ btn.addEventListener("click", (e) => {
   let val = inp.value; //  val get a string value here
   val = Number(val); // here this string value is changed to a number
 
+  if (!val || val < 1 || val > 100) {
+    alert("Please enter a valid number between 1 and 100");
+    return;
+  }
+
   res.className = res.className.replace("hidden", "").trim(); // remove "hidden" class
   res.className += " block"; // add class block
 
   if (gv) {
     const guess_heading = document.createElement("h3");
     guess_heading.innerHTML = "Guess Values";
-
+    guess_heading.style.textAlign = "center";
+    guess_heading.style.marginBottom = "1.25rem";
+    guess_heading.style.fontWeight = "bold";
     res.appendChild(guess_heading);
     let attempts_msg = document.createElement("p");
     attempts_msg.innerHTML = `Total attempts ${attempts}`;
@@ -34,7 +40,6 @@ btn.addEventListener("click", (e) => {
   if (val === randno) {
     let elem = document.createElement("p");
     elem.innerHTML = "Right Guess";
-
     btn.disabled = true;
     inp.disabled = true;
     res.appendChild(elem);
@@ -42,12 +47,12 @@ btn.addEventListener("click", (e) => {
   } else if (val > randno) {
     let elem = document.createElement("p");
     attempts -= 1;
-    elem.innerHTML = `Too small ${val} <br> remaining attempts ${attempts}`;
+    elem.innerHTML = `Too large ${val} <br> remaining attempts ${attempts}`;
     res.appendChild(elem);
   } else {
     let elem = document.createElement("p");
     attempts -= 1;
-    elem.innerHTML(`Too small ${val} <br> remaining attempts ${attempts}`);
+    elem.innerHTML = `Too small ${val} <br> remaining attempts ${attempts}`;
     res.appendChild(elem);
   }
   if (attempts === 0) {
